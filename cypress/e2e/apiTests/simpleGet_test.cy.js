ed72f84d4227d1f798dc242ae0f36cf20b359253
irsadartus <erxat413@gmail.com>
2023-07-18 14:26:30 +0300

    Update

--- cypress/e2e/apiTests/simpleGet_test.cy.js
+++ cypress/e2e/apiTests/simpleGet_test.cy.js
@@ -1,19 +1,16 @@
 
-describe('How to do API tests with cypress' , ()=> {
-
-    it('Simple GET request , check status headers and body' , ()=> {
-        cy.request( {
-            // this function takes a json object as parameter , and inside 
-            // define core parts of HTTP request
-
-            method: 'GET' , 
-            // hardcoded url : https://demoqa.com/BookStore/v1/Books
-            url: `${Cypress.env.apply('apiUrl')}${Cypress.env('apiBooks')}` , 
-            // other than method and url the rest of options depend on your test case
-            failOnStatusCode : false
-        }).then((response) => {
-            expect(response.status).to.equal(400) ; 
-        })
-
-    })
-})
+describe('How to do API tests with cypress', () => {
+    it('Simple GET request, check status headers and body', () => {
+      cy.request({
+        // this function takes a json object as parameter, and inside this object we define core parts of HTTP request
+        method: 'GET',
+        // hardcoded url: https://demoqa.com/BookStore/v1/Books
+        url: `${Cypress.env('apiUrl')}${Cypress.env('apiBooks')}`,
+        // other than method and url the rest of options depend on your test case
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+       
+      });
+    });
+  });
